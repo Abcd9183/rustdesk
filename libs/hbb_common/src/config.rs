ee3fab4ad4528df03aee637fb57e76d23d603606
Abcd9183 <as-dftyui@163.com>
2024-09-05 00:59:00 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -101,7 +101,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["74391l8b3.zicp.vip"];
-pub const PUBLIC_RS_PUB_KEY: &str = "vZ0ESrvC+HlRNrpiUZWMZ01n3GlNMDDtOk0oA2SnCwM=";
+pub const PUBLIC_RS_PUB_KEY: &str = "smYtrNm0pgHmpPZXm1KzVIjIWTPABJyeYRzYjamX2Yg=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
